25ca1ec3ba95ca78bd723c4479a1fe8e18a474b1
lphgor <3890346+lphgor@users.noreply.github.com>
2019-12-31 19:59:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Update kcptun
-sed -i 's/20190515/20191219/g' ./package/lean/kcptun/Makefile
+sed -i 's/20190515/20191229/g' ./package/lean/kcptun/Makefile
 
 # Enable 55r
 echo 0xDEADBEEF > ./package/lean/luci-app-ssr-plus/root/etc/config/google_fu_mode
