4dc79ecbda4aa028e79745e4ccbd7fc43648988e
lphgor <3890346+lphgor@users.noreply.github.com>
2020-03-23 19:52:56 +0800

    build
    Signed-off-by: lphgor <3890346+lphgor@users.noreply.github.com>

---
+++ diy1907.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Update shadowsocks-libev
+sed -i 's/3.2.5/3.3.4/g;s/02273d571b8bea067eefb203a43c142d052042179904f9050a1f20cb8dc5d242/fce47a956fad0c30def9c71821bcec450a40d3f881548e31e66cedf262b89eb1/g' ./feeds/packages/net/shadowsocks-libev/Makefile
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+
+sed -i 's#http://downloads.openwrt.org/releases/19.07-SNAPSHOT#http://downloads.openwrt.org/releases/19.07.2#g' ./include/version.mk
